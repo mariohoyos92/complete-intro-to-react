89ecf9f610940affc1fb669c593cbbe18f9713d3
Mario Hoyos <mariohoyos92@gmail.com>
2018-01-17 14:11:22 -0600

    feat: search bar input modifiable

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+
 import preload from "../data.json";
+import ShowCard from "./ShowCard";
 
-import ShowCard from "./ShowCard.jsx";
-
-const Search = () => (
-  <div className="search">
-    {preload.shows.map(show => <ShowCard show={show} />)}
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "some sort of debug statement"
+    };
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+  handleSearchTermChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="search"
+          />
+        </header>
+        <div>
+          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
